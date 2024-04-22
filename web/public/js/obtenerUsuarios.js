d31390c8ba8b368774bf78d5d7c5faa36298e3ea
MiguelPayan <l20550629@chihuahua2.tecnm.mx>
2024-04-22 14:49:11 -0600

    El boton 'Eliminar ' obtiene solo el correo

--- web/public/js/obtenerUsuarios.js
+++ web/public/js/obtenerUsuarios.js
@@ -3,11 +3,24 @@
 
 const container = document.querySelector(".container");
 
+function eliminar(boton){
+    // Obtener el elemento padre (el <li>) del botón clicado
+  let liElemento = boton.parentNode;
+
+  // Obtener el contenido del <li>
+  let contenido = liElemento.textContent.trim(); // Eliminar espacios en blanco al inicio y final
+
+  // Guardar el contenido en la variable
+  const textoDividido = contenido.split('✉️');
+  const correo = textoDividido[0];
+  // Mostrar el contenido guardado (solo para demostración)
+  console.log(correo);
+}
 
 fetch('https://backstoreyourmusic.onrender.com/usuarios')
 .then((response) => response.json())
 .then((users)=>{
-    let ptl = users.map(user => `<li>${user.email} ✉️ ${user.name}</li>`);
+    let ptl = users.map(user => `<li>${user.email} ✉️ ${user.name} <button onclick="eliminar(this)">Eliminar</button></li>`);
     ptl = ptl.join('');
     container.innerHTML = `<ul>${ptl}</ul>`;
     
