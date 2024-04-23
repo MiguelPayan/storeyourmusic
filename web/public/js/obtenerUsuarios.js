82605587634c0538e2a4a768ed03ad8a9c5e0ef2
MiguelPayan <l20550629@chihuahua2.tecnm.mx>
2024-04-22 18:18:53 -0600

    Borrar usuarios listo

--- web/public/js/obtenerUsuarios.js
+++ web/public/js/obtenerUsuarios.js
@@ -3,7 +3,7 @@
 
 const container = document.querySelector(".container");
 
-function eliminar(boton){
+async function eliminar(boton){
     // Obtener el elemento padre (el <li>) del botón clicado
   let liElemento = boton.parentNode;
 
@@ -15,10 +15,34 @@ function eliminar(boton){
   const correo = textoDividido[0];
   // Mostrar el contenido guardado (solo para demostración)
   console.log(correo);
-}
 
-fetch('https://backstoreyourmusic.onrender.com/usuarios')
-.then((response) => response.json())
+  try {
+    const response = await fetch('https://backstoreyourmusic.onrender.com/eliminaruser', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        'Access-Control-Allow-Origin':'*',
+        'Access-Control-Allow-Methods':'GET,POST,PATCH,OPTIONS'
+      },
+      body: JSON.stringify({ email: correo })
+    });
+
+    if (response.ok) {
+      console.log("Usuario eliminado correctamente");
+    } else {
+      console.error("Error al eliminar usuario");
+    }
+  } catch (error) {
+    console.error("Error de red:", error);
+  }
+
+}
+let headers = new Headers();
+headers.append('Access-Control-Allow-Origin', 'http://localhost:5500');
+headers.append('Access-Control-Allow-Credentials', 'true');
+
+fetch('https://backstoreyourmusic.onrender.com/usuarios', {headers: headers})
+.then((response) =>response.json())
 .then((users)=>{
     let ptl = users.map(user => `<li>${user.email} ✉️ ${user.name} <button onclick="eliminar(this)">Eliminar</button></li>`);
     ptl = ptl.join('');
