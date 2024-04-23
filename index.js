8aa5bea44999fff3d53e9fc47c8c70c383b4616b
MiguelPayan <l20550629@chihuahua2.tecnm.mx>
2024-04-22 19:25:25 -0600

    Se agrego la obligacion de usar la cuenta admin para ver a los usuarios

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 let credenciales = sessionStorage.getItem("token");
-console.log('Importado desde ./index.js');
-function parseJwt (token) {
-    var base64Url = token.split('.')[1];
-    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''));
+btn_usuarios = document.getElementById("usuarios");
 
-    return JSON.parse(jsonPayload);
+function parseJwt (token) {
+    try{
+        var base64Url = token.split('.')[1];
+        var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
+    
+        return JSON.parse(jsonPayload);
+    }catch(e){console.log(e)}
+    
+    
+    
 }
 
 if(credenciales == null){
@@ -23,13 +29,43 @@ if(credenciales == null){
     };
 }
 
-credenciales = parseJwt(credenciales);
-console.log(credenciales);
-const imagenPerfil = document.createElement("img")
-imagenPerfil.src=credenciales.picture;
-imagenPerfil.className="imagenPerfil";
-console.log(`Imagen de perfil en: ${credenciales.picture}`)
-const loginButton = document.querySelector(".btn-login");
+try{
+    credenciales = parseJwt(credenciales);
+    const imagenPerfil = document.createElement("img")
+    imagenPerfil.src=credenciales.picture;
+    imagenPerfil.className="imagenPerfil";
+    console.log(`Imagen de perfil en: ${credenciales.picture}`)
+    const loginButton = document.querySelector(".btn-login");
+    
+    loginButton.textContent=`Sesion iniciada como ${credenciales.name}`;
+    loginButton.appendChild(imagenPerfil);
+}catch(e){console.log(e)}
 
-loginButton.textContent=`Sesion iniciada como ${credenciales.name}`;
-loginButton.appendChild(imagenPerfil);
+
+
+btn_usuarios.addEventListener('click', function(event) {
+    event.preventDefault(); // Evita la acción por defecto del enlace (navegar a otra página)
+    console.log('Enlace clickeado');
+
+    if(credenciales == null){
+        alert("Inicia sesion con la cuenta de administrador")
+    }
+    return true;
+  });
+
+  
+try{
+    if (credenciales.email != null && "miguelelcracked@gmail.com" == credenciales.email){
+        console.log("hola")
+        
+        btn_usuarios.onclick = function usuariosRegistrados(){
+            location.href("/public/html/usuarios.html");
+        }
+    }else{
+        btn_usuarios.style.display = "none";
+    }
+}catch(e){
+    console.log(e)
+}
+
+
