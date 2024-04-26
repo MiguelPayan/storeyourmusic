73bfd08c0d5588bce12217e87319f4cdd59e9cf4
CarlosGuardado <L20550382@chihuahua2.tecnm.mx>
2024-04-26 02:34:53 -0600

    a

---
+++ my-react-app/src/Menu.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../../web/css/Style.css';
+
+function Menu() {
+  return (
+    <div>
+      <input type="checkbox" id="active" />
+      <label htmlFor="active" className="menu-btn">
+        <img src="/web/images/nav icon.png" alt="Icono de menú" />
+        <i className="fas fa-bars"></i>
+      </label>
+      <div className="wrapper">
+        <ul>
+          <li><a href="#">Inicio</a></li>
+          <li><a href="#">Canciones</a></li>
+          <li><a href="#" id="cerrar_sesion">Cerrar Sesión</a></li>
+          <li><a href="#" id="usuarios">Usuarios Registrados</a></li>
+          <li><a href="login" className="btn-login">Iniciar Sesión</a></li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Menu;
