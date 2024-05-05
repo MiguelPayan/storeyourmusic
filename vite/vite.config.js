b834eabc5f46af09a33f524a23e75af70e3c6598
CarlosGuardado <L20550382@chihuahua2.tecnm.mx>
2024-05-04 22:45:43 -0600

    a

--- vite/vite.config.js
+++ vite/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import react from '@vitejs/plugin-react' // Agrega esta línea
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    react(), // Agrega esta línea
+  ],
 })
