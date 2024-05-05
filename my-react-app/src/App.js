275120a9dd8fc6dc2d298f13223af882a4643e07
CarlosGuardado <L20550382@chihuahua2.tecnm.mx>
2024-05-04 22:04:36 -0600

    a

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,28 +1,12 @@
 import React from 'react';
-import Menu from './Menu'; // Importa el componente Menu
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      {/* Renderiza el componente Menu */}
-      <Menu />
+      {/* No es necesario renderizar el componente Menu */}
       
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* No necesitas el header predeterminado */}
     </div>
   );
 }
--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -8,9 +8,9 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  // En lugar de document.getElementById('root'), simplemente pasamos null
-  // ya que el elemento 'root' se maneja automáticamente en el archivo index.html en la carpeta public
-  document.getElementById('root')
+  // No es necesario pasar ningún elemento como segundo argumento
+  // React se encargará automáticamente de renderizar en el archivo index.html en la carpeta public
+  null
 );
 
 // Si deseas medir el rendimiento en tu aplicación, puedes dejar esta parte.
