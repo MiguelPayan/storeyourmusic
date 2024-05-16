181bc33cd9e88ad8ce01a0636adfa569aa1da567
MiguelPayan <l20550629@chihuahua2.tecnm.mx>
2024-05-16 00:25:54 -0600

    Se agrego la primer cancion para reproducir

--- web/public/js/miperfil.js
+++ web/public/js/miperfil.js
@@ -1,32 +1,52 @@
-let credenciales = sessionStorage.getItem("token");
+try{
+    let credenciales = sessionStorage.getItem("token");
 
-let valorNombre, valorCorreo;
-
-function parseJwt (token) {
-    try{
-        var base64Url = token.split('.')[1];
-        var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-        var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
-            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-        }).join(''));
+    let valorNombre, valorCorreo;
     
-        return JSON.parse(jsonPayload);
-    }catch(e){console.log(e)}
+    function parseJwt (token) {
+        try{
+            var base64Url = token.split('.')[1];
+            var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+            var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+                return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+            }).join(''));
+        
+            return JSON.parse(jsonPayload);
+        }catch(e){console.log(e)}
+    }
+    
+    credenciales = parseJwt(credenciales);
+    
+    console.log(credenciales)
+    
+    playButton = document.getElementsByClassName("play")[0];
+    pauseButton = document.getElementsByClassName("pause")[0];
+    nombre = document.getElementsByClassName("valor")[0];
+    correo = document.getElementsByClassName("valor")[1];
+    console.log(correo)
+    nombre_header = document.getElementById("nombre-header");
+    
+    valorNombre =credenciales.name;
+    valorCorreo = credenciales.email;
+    
+    nombre_header.textContent = valorNombre; 
+    nombre.textContent = valorNombre;
+    correo.textContent = valorCorreo;
+}catch(error){
+    console.log("Error con la sesion");
+    console.log(error);
 }
 
-credenciales = parseJwt(credenciales);
 
-console.log(credenciales)
 
-nombre = document.getElementsByClassName("valor")[0];
-correo = document.getElementsByClassName("valor")[1];
-console.log(correo)
-nombre_header = document.getElementById("nombre-header");
+// Obtener el elemento de audio
+let miAudio = document.getElementById('mi_audio');
 
-valorNombre =credenciales.name;
-valorCorreo = credenciales.email;
 
-nombre_header.textContent = valorNombre; 
-nombre.textContent = valorNombre;
-correo.textContent = valorCorreo;
+function play(){
+    miAudio.play();
+}
+function pause(){
+    miAudio.pause();
+}
 
