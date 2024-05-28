5f297e3d5292fd2fdd698d9a1225c41ae35fadaa
CarlosGuardado <L20550382@chihuahua2.tecnm.mx>
2024-05-27 20:17:07 -0600

    Ajuste de foto en menu

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 let credenciales = sessionStorage.getItem("token");
-btn_usuarios = document.getElementById("usuarios");
+let btn_usuarios = document.getElementById("usuarios");
 
-function parseJwt (token) {
-    try{
+function parseJwt(token) {
+    try {
         var base64Url = token.split('.')[1];
         var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
         var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
@@ -10,65 +10,61 @@ function parseJwt (token) {
         }).join(''));
     
         return JSON.parse(jsonPayload);
-    }catch(e){console.log(e)}
-    
-    
-    
+    } catch (e) {
+        console.log(e);
+    }
 }
 
-if(credenciales == null){
-    btn_cerrar_sesion = document.getElementById("cerrar_sesion");
-    btn_cerrar_sesion.style.display = "none"
+if (credenciales == null) {
+    let btn_cerrar_sesion = document.getElementById("cerrar_sesion");
+    btn_cerrar_sesion.style.display = "none";
 
-    miPerfil = document.getElementById("mi_perfil");
-    miPerfil.style.display = "none"
-}else{
-    btn_cerrar_sesion = document.getElementById("cerrar_sesion");
+    let miPerfil = document.getElementById("mi_perfil");
+    miPerfil.style.display = "none";
+} else {
+    let btn_cerrar_sesion = document.getElementById("cerrar_sesion");
     btn_cerrar_sesion.style.display = "inline-block";
-    btn_cerrar_sesion.onclick = function cerrarSesion (){
+    btn_cerrar_sesion.onclick = function cerrarSesion() {
         sessionStorage.removeItem("token");
         credenciales = null;
         location.reload();
     };
 }
 
-try{
+try {
     credenciales = parseJwt(credenciales);
-    const imagenPerfil = document.createElement("img")
-    imagenPerfil.src=credenciales.picture;
-    imagenPerfil.className="imagenPerfil";
-    console.log(`Imagen de perfil en: ${credenciales.picture}`)
-    const loginButton = document.querySelector(".btn-login");
+    const imagenPerfil = document.createElement("img");
+    imagenPerfil.src = credenciales.picture;
+    imagenPerfil.className = "imagenPerfil";
+    console.log(`Imagen de perfil en: ${credenciales.picture}`);
     
-    loginButton.textContent=`Sesion iniciada como ${credenciales.name}`;
+    const loginButton = document.querySelector(".btn-login");
+    loginButton.textContent = `Sesión iniciada como ${credenciales.name}`;
     loginButton.appendChild(imagenPerfil);
-}catch(e){console.log(e)}
-
-
-
-btn_usuarios.addEventListener('click', function(event) {
-    event.preventDefault(); // Evita la acción por defecto del enlace (navegar a otra página)
-    console.log('Enlace clickeado');
-
-    if(credenciales == null){
-        alert("Inicia sesion con la cuenta de administrador")
-    }
-    return true;
-  });
-
-  
-try{
-    if (credenciales.email != null && "thegigaboss57@gmail.com" == credenciales.email){
-        console.log("hola")
-        
-        btn_usuarios.onclick = function usuariosRegistrados(){
-            window.location = ("/web/public/html/usuarios.html");
-        }
-    }else{
-        btn_usuarios.style.display = "none";
-    }
-}catch(e){
-    console.log(e)
+} catch (e) {
+    console.log(e);
 }
 
+btn_usuarios.addEventListener('click', function(event) {
+    event.preventDefault();
+    console.log('Enlace clickeado');
 
+    if (credenciales == null) {
+        alert("Inicia sesión con la cuenta de administrador");
+    }
+    return true;
+});
+
+try {
+    if (credenciales.email != null && "thegigaboss57@gmail.com" == credenciales.email) {
+        console.log("hola");
+        
+        btn_usuarios.onclick = function usuariosRegistrados() {
+            window.location = ("/web/public/html/usuarios.html");
+        }
+    } else {
+        btn_usuarios.style.display = "none";
+    }
+} catch (e) {
+    console.log(e);
+}
